ed3cbdf7669943dc9708f968345767a12ad1dbca
LSH-3016 <a673813@naver.com>
2022-07-22 01:34:31 +0900

    숫자 확인 클래스 분리

---
+++ src/main/java/calculator/numberValidation.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import calculatorIO.validity;
+import error.NumberError;
+
+public class numberValidation implements validity {
+
+    @Override
+    public void doValidation(String data) {
+        try{
+            Integer.parseInt(data);
+        } catch(NumberFormatException e){
+            new NumberError().printErrorMessage();
+            System.exit(0);
+        }
+    }
+}
