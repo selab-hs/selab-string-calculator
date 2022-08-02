c708f68283cbfd1fc7d08696c81129c6c53e142c
LSH-3016 <a673813@naver.com>
2022-08-02 21:00:50 +0900

    숫자 확인을 만을 하게끔 기능 변경

--- src/main/java/calculator/NumberValidation.java
+++ src/main/java/calculator/NumberValidation.java
@@ -1,17 +1,16 @@
 package calculator;
 
 import calculatorIO.validity;
-import error.NumberError;
 
 public class NumberValidation implements validity {
 
     @Override
-    public void doValidation(String data) {
+    public boolean doValidation(String data) {
         try{
             Integer.parseInt(data);
+            return true;
         } catch(NumberFormatException e){
-            new NumberError().printErrorMessage();
-            System.exit(0);
+            return false;
         }
     }
 }
