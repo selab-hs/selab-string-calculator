774e1df5a9cf0ebd817d81961bdbb7ee4459e4a6
LSH-3016 <a673813@naver.com>
2022-08-02 21:00:19 +0900

    객체로써의 역할만 하게끔 전반적인 구조 변경

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,23 +1,11 @@
 package calculator;
-
-import calculatorIO.validity;
-import error.NumberError;
-import calculator.numberValidation;
-
 public class Number {
 
     private final int number;
 
-    public Number(Calculator calculator, String number){
-        new numberValidation().doValidation(number);
+    public Number(String number){
         this.number = Integer.parseInt(number);
-        addNumber(calculator,this);
     }
-
-    private void addNumber(Calculator calculator,Number number){
-        calculator.number.add(number);
-    }
-
     public int getNumber(){
         return this.number;
     }
