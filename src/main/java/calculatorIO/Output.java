8ab5792c4ba92bb861cafd808b7314472eaaaa5d
LSH-3016 <a673813@naver.com>
2022-08-02 21:02:35 +0900

    출력 변경

--- src/main/java/calculatorIO/Output.java
+++ src/main/java/calculatorIO/Output.java
@@ -3,14 +3,12 @@ package calculatorIO;
 import calculator.Calculator;
 
 public class Output {
-    private final String OUTPUT_MESSAGE = "계산 결과 : ";
-
     public Output(Calculator calculator){
         printOutputMessage();
         System.out.println(calculator.getResult());
     }
 
     public void printOutputMessage(){
-        System.out.print("\n" + OUTPUT_MESSAGE);
+        System.out.print("\n계산 결과 : ");
     }
 }
