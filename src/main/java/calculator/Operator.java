7b9630ceda61966939e5f7ca690869e79020941e
LSH-3016 <a673813@naver.com>
2022-08-02 21:01:07 +0900

    객체로써의 역할만 하게끔 전반적인 구조 변경

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,21 +1,10 @@
 package calculator;
-
-import calculation.Plus;
-import calculator.operatorValidation;
-import error.OperatorError;
-
 public class Operator{
 
     private final String operator;
 
-    public Operator(Calculator calculator, String operator){
-        new operatorValidation().doValidation(operator);
+    public Operator(String operator){
         this.operator = operator;
-        addOperator(calculator,this);
-    }
-
-    private void addOperator(Calculator calculator,Operator operator){
-        calculator.operator.add(operator);
     }
 
     public String getOperator(){
