2f27179ecd7b9646ba334c6e24bab163916f401a
LSH-3016 <a673813@naver.com>
2022-08-02 20:53:58 +0900

    메인이 아닌 enum 클래스를 참조하도록 변경

--- src/main/java/calculation/Calculation.java
+++ src/main/java/calculation/Calculation.java
@@ -1,35 +1,34 @@
 package calculation;
 
 import calculator.Calculator;
+import calculator.OperatorValidation;
 
 public class Calculation {
 
-    private int count;
-    private int size;
 
     public Calculation(Calculator calculator){
 
-        size = calculator.number.size();
 
-        for (count = 0; count < size; count++){
-            if(count == 0){
+        int count;
+        int size = calculator.number.size();
+
+        for (count = 0; count < size; count++) {
+            if (count == 0) {
                 calculator.setResult(calculator.number.poll().getNumber());
                 continue;
             }
-
             String operator = calculator.operator.poll().getOperator();
-
-            if(operator.equals(calculator.PLUS.getSIGN())){
-                calculator.setResult(calculator.PLUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
+            if (operator.equals(OperatorValidation.PLUS.getSign())) {
+                calculator.setResult(OperatorValidation.PLUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
             }
-            if(operator.equals(calculator.MINUS.getSIGN())){
-                calculator.setResult(calculator.MINUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
+            if (operator.equals(OperatorValidation.MINUS.getSign())) {
+                calculator.setResult(OperatorValidation.MINUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
             }
-            if(operator.equals(calculator.TIMES.getSIGN())){
-                calculator.setResult(calculator.TIMES.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
+            if (operator.equals(OperatorValidation.TIMES.getSign())) {
+                calculator.setResult(OperatorValidation.TIMES.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
             }
-            if(operator.equals(calculator.DIVISION.getSIGN())){
-                calculator.setResult(calculator.DIVISION.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
+            if (operator.equals(OperatorValidation.DIVISION.getSign())) {
+                calculator.setResult(OperatorValidation.DIVISION.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
             }
         }
     }
