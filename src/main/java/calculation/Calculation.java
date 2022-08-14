8ded93051613cbd4ff4feb978302e66f3925f882
LSH-3016 <a673813@naver.com>
2022-08-15 03:36:14 +0900

    enum과 람다식을 이용해 코드 간략화

--- src/main/java/calculation/Calculation.java
+++ src/main/java/calculation/Calculation.java
@@ -1,35 +1,26 @@
 package calculation;
 
-import calculator.Calculator;
-import calculator.OperatorValidation;
+import calculator.*;
+
+import java.util.Iterator;
 
 public class Calculation {
-
-
+    
     public Calculation(Calculator calculator){
+        calculator.setResult(calculator.number.poll().getNumber());
 
+        Iterator max = calculator.operator.iterator();
 
-        int count;
-        int size = calculator.number.size();
-
-        for (count = 0; count < size; count++) {
-            if (count == 0) {
-                calculator.setResult(calculator.number.poll().getNumber());
-                continue;
-            }
-            String operator = calculator.operator.poll().getOperator();
-            if (operator.equals(OperatorValidation.PLUS.getSign())) {
-                calculator.setResult(OperatorValidation.PLUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
-            }
-            if (operator.equals(OperatorValidation.MINUS.getSign())) {
-                calculator.setResult(OperatorValidation.MINUS.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
-            }
-            if (operator.equals(OperatorValidation.TIMES.getSign())) {
-                calculator.setResult(OperatorValidation.TIMES.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
-            }
-            if (operator.equals(OperatorValidation.DIVISION.getSign())) {
-                calculator.setResult(OperatorValidation.DIVISION.doCalculate(calculator.getResult(), calculator.number.poll().getNumber()));
-            }
+        while(max.hasNext()){
+            calculator.setResult(
+                    Operator
+                    .getOperator(calculator.operator.poll())
+                    .doCalculate(calculator.getResult(), calculator.number.poll().getNumber())
+            );
         }
+
+
     }
+
+
 }
