d768e848bdcd87f38dbbfde2cbe2d08cd4563d53
LSH-3016 <a673813@naver.com>
2022-08-15 03:52:25 +0900

    기존의 동일명 클래스 삭제 및 enum으로 대체, 필요 메소드 작성

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,14 +1,37 @@
 package calculator;
-public class Operator{
 
-    private final String operator;
+import java.util.Arrays;
+import java.util.function.BiFunction;
 
-    public Operator(String operator){
-        this.operator = operator;
+public enum Operator {
+    PLUS("+", (number1, number2) -> number1 + number2),
+    MINUS("-", (number1, number2) -> number1 - number2),
+    TIMES("*", (number1, number2) -> number1 * number2),
+    DIVISION("/", (number1, number2) -> number1 / number2);
+    private final String sign;
+    private final BiFunction<Integer, Integer, Integer> doCalculate;
+
+    Operator(String sign, BiFunction<Integer, Integer, Integer> doCalculate) {
+        this.sign = sign;
+        this.doCalculate = doCalculate;
     }
 
-    public String getOperator(){
-        return this.operator;
+    public static boolean isOperator(String data) {
+        return Arrays.stream(values())
+                .anyMatch(operator -> compareSign(operator, data));
+    }
+    private static boolean compareSign(Operator operator, String data){
+        return operator.sign.equals(data);
     }
 
+    public static Operator getOperator(String data){
+        return Arrays.stream(values())
+                .filter(operator -> compareSign(operator, data))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 연산자를 입력하셨습니다."));
+    }
+
+    public int doCalculate(int number1, int number2){
+        return doCalculate.apply(number1, number2);
+    }
 }
