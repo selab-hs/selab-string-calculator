cdcacf762b210b10eb5ed8f39389699f20ae38ba
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-08-15 22:18:14 +0900

    리턴값 수정

--- src/main/java/inputlogic/InputBufferReader.java
+++ src/main/java/inputlogic/InputBufferReader.java
@@ -13,20 +13,18 @@ import java.util.List;
 public class InputBufferReader implements  InputLogic{
     List<String> formula = new ArrayList<>();
     @Override
-    public void inputFormula() {
+    public List<String> inputFormula() {
         try{
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             String str = bufferedReader.readLine();
             this.formula = Arrays.asList(str.split(" "));
             bufferedReader.close();
+            return this.formula;
         }catch (IOException e){
             e.printStackTrace();
             System.out.println(e.getMessage());
             inputFormula();
         }
-    }
-
-    public List<String> getFactoredFormula() {
-        return formula;
+        return new ArrayList<>();
     }
 }
