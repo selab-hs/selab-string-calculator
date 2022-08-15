6f98a9b88f2a032c2672e65cef6d28eb2d9aa83b
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-08-15 22:17:10 +0900

    NullPointException 수정

--- src/main/java/sequencelogic/MainSequence.java
+++ src/main/java/sequencelogic/MainSequence.java
@@ -2,18 +2,22 @@ package sequencelogic;
 
 import calculatelogic.CalculateConfig;
 import inputlogic.InputConfig;
+import showconsole.ShowAnswer;
 import storage.Formula;
+import validatelogic.sequence.ValidateConfig;
 
 public class MainSequence {
     static public void sequence(){
+
         InputConfig inputConfig = new InputConfig();
-        inputConfig.inputLogic().inputFormula();
-
+        ValidateConfig validateConfig = new ValidateConfig();
         Formula formula = new Formula();
-
-        formula.setFormula(inputConfig.inputLogic().getFactoredFormula());
-
         CalculateConfig calculateConfig = new CalculateConfig();
-        calculateConfig.calculateLogic().calculateAll(formula.getFormula());
+        ShowAnswer showAnswer = new ShowAnswer();
+
+
+        formula.setFormula(inputConfig.inputLogic().inputFormula());
+        validateConfig.validateSequence().order(formula.getFormula());
+        showAnswer.showResult(calculateConfig.calculateLogic().calculateAll(formula.getFormula()));
     }
 }
