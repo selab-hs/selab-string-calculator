651e0f4c6402b9e7c52f5467c456d38f288dae6b
LSH-3016 <a673813@naver.com>
2022-08-16 00:17:31 +0900

    생성자 제거

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,11 +1,7 @@
 package view;
 
 public class Output {
-    public Output(int result){
-        printOutputMessage(result);
-    }
-
-    public void printOutputMessage(int result){
+    public void printOutput(int result) {
         System.out.print("\n계산 결과 : " + result);
     }
 }
