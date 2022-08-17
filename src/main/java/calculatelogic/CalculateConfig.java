38d4ef4c0c9a2881085566cb487ee309178601bf
usr <rlarlgus0206@naver.com>
2022-08-17 12:10:37 +0900

    메소드 책임 분리

--- src/main/java/calculatelogic/CalculateConfig.java
+++ src/main/java/calculatelogic/CalculateConfig.java
@@ -5,6 +5,10 @@ import calculatelogic.nomallogic.NormalCalculateLogic;
 public class CalculateConfig {
 
     public CalculateLogic calculateLogic(){
+        return logic();
+    }
+
+    private CalculateLogic logic(){
         return new NormalCalculateLogic();
     }
 }
