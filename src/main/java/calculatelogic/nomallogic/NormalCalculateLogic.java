d74f8b2697512c6dcda64d806d5c4128e409327c
usr <rlarlgus0206@naver.com>
2022-08-17 12:11:44 +0900

    클래스를 두 개를 하나로 합치며 클래스의 수 단축

--- src/main/java/calculatelogic/nomallogic/NormalCalculateLogic.java
+++ src/main/java/calculatelogic/nomallogic/NormalCalculateLogic.java
@@ -2,17 +2,27 @@ package calculatelogic.nomallogic;
 
 
 import calculatelogic.CalculateLogic;
+import storage.AnalyzeSignAndCalculate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class NormalCalculateLogic implements CalculateLogic {
-    OperatorMap operatorMap = new OperatorMap();
 
+    private static Map<String, AnalyzeSignAndCalculate> operatorMap = new HashMap<>();
+
+    static {
+        operatorMap.put("+", AnalyzeSignAndCalculate.PLUS);
+        operatorMap.put("-", AnalyzeSignAndCalculate.MINUS);
+        operatorMap.put("*", AnalyzeSignAndCalculate.MULTIPLY);
+        operatorMap.put("/", AnalyzeSignAndCalculate.DIVIDE);
+    }
     int answer;
 
     public double calculateSinglePart(int a, String sign, int b) {
-        return Optional.ofNullable(operatorMap.getOperatorMap().get(sign))
+        return Optional.ofNullable(operatorMap.get(sign))
                 .orElseThrow(() -> new IllegalArgumentException("error"))
                 .calculate(a, b);
     }
