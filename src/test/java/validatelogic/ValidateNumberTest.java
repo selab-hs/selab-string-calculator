f0bc1ef6fba1bf978a2fc5ccb19aa94db74297da
usr <rlarlgus0206@naver.com>
2022-08-17 12:14:01 +0900

    테스트 코드 수정

---
+++ src/test/java/validatelogic/ValidateNumberTest.java
@@ -0,0 +1,53 @@
+package validatelogic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateNumberTest {
+    List<String> formula1 = Arrays.asList(
+            "10",
+            "+",
+            "30"
+    );
+
+    List<String> wrongNumbers = Arrays.asList(
+            "02",
+            "003",
+            "03",
+            "0"
+    );
+
+    @Test
+    public void checkValidate() {
+
+        List<String> extractFormula = new ArrayList<>();
+
+        formula1.stream()
+                .filter(n -> n.matches("[0-9]+"))
+                .forEach(extractFormula::add);
+
+        for (int i = 0; i < extractFormula.size(); i++) {
+            Assertions.assertTrue(Integer.parseInt(extractFormula.get(i)) > 0);
+        }
+    }
+
+    @Test
+    public void notStartZero() {
+        wrongNumbers.stream()
+                .forEach(ValidateNumberTest::notStartZero);
+
+    }
+
+    private static void notStartZero(String number) {
+        if (number.charAt(0) == '0' && !number.equals("0")) {
+            System.out.println("Error");
+        }
+    }
+
+}
---
+++ src/test/java/validatelogic/ValidateSignTest.java
@@ -0,0 +1,26 @@
+package validatelogic;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateSignTest {
+
+    String validateSign = "+ / * -";
+
+    List<String> testSign  = Arrays.asList(
+            "+",
+            "-",
+            "/",
+            "*"
+    );
+
+    @Test
+    void checkValidate() {
+        testSign.stream()
+                .forEach(n -> assertTrue(validateSign.contains(n)));
+    }
+}
