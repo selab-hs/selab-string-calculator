5ff31adfa7af8949adf819037fa6dc17b4fe0973
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-09-06 22:00:17 +0900

    답 출력 클래스 재작성

--- src/main/java/showconsole/AnswerNotification.java
+++ src/main/java/showconsole/AnswerNotification.java
@@ -1,6 +1,12 @@
 package showconsole;
 
 public class AnswerNotification {
+
+    public static AnswerNotification answerNotification = new AnswerNotification();
+
+    private AnswerNotification(){
+
+    }
     public void expressResult(int answer){
         System.out.println("Answer : " +answer);
     }
