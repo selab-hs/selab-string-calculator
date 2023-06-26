4281a49e7ba493836e9b6d1e120d9bd92f224507
ds4pae <ds4pae@gmail.com>
2023-06-26 17:48:43 +0900

    불필요한 import 제거

--- src/main/java/CheckNumOperator.java
+++ src/main/java/CheckNumOperator.java
@@ -1,7 +1,4 @@
 import java.util.ArrayList;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 
 public class CheckNumOperator {
     protected static ArrayList<String> inputOperator = new ArrayList<>();
