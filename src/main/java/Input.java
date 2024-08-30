af42101b3759eccd7ec96aaa81b20035a0ddb2e5
kangs <sangmoonkang@naver.com>
2024-08-30 22:49:40 +0900

    DELETE(Calculator) : 코드 수정을 통해 삭제

--- src/main/java/Input.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Input {
-    public static String[] readInput(Scanner scanner) {
-        System.out.print("수식을 입력하세요: ");
-        return scanner.nextLine().split(" ");
-    }
-}
--- src/main/java/Output.java
+++
@@ -1,6 +0,0 @@
-public class Output {
-
-    public static void printResult(int result) {
-        System.out.println("결과: " + result);
-    }
-}
