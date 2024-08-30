261ce04542fe76966175f24c168826b248df86f6
kangs <sangmoonkang@naver.com>
2024-08-30 22:43:32 +0900

    ADD(Calculator) : 계산기 수식 입력 처리 코드

---
+++ src/main/java/calculator/view/InputView.java
@@ -0,0 +1,11 @@
+package calculator.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public String[] formulaInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("수식을 입력하세요 : ");
+        return scanner.nextLine().split(" ");
+    }
+}
