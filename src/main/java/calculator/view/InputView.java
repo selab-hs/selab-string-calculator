56aac5c54d80cbbf9dc4b2004a2154c8e09d97f8
kangs <sangmoonkang@naver.com>
2024-08-31 01:50:29 +0900

    refactor(Calculator) : 문자열을 반환하도록 수정
    파싱 로직을 FormulaParser를 추가하여 책임 분리

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,9 +3,9 @@ package calculator.view;
 import java.util.Scanner;
 
 public class InputView {
-    public String[] formulaInput() {
+    public String formulaInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("수식을 입력하세요 : ");
-        return scanner.nextLine().split(" ");
+        return scanner.nextLine();
     }
 }
