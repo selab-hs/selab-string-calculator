9d312d6e7f8a582907398e397987d70a8a1df709
kangs <sangmoonkang@naver.com>
2024-08-31 00:20:19 +0900

    refactor(Calculator) : CalculatorController에 FormulaParser 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,6 +1,7 @@
 package calculator.controller;
 
 import calculator.model.Operator;
+import calculator.util.FormulaParser;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
@@ -8,15 +9,18 @@ public class CalculatorController {
     private final Operator operator;
     private final InputView inputView;
     private final OutputView outputView;
+    private final FormulaParser formulaParser;
 
-    public CalculatorController(Operator operator, InputView inputView, OutputView outputView) {
+    public CalculatorController(Operator operator, InputView inputView, OutputView outputView, FormulaParser formulaParser) {
         this.operator = operator;
         this.inputView = inputView;
         this.outputView = outputView;
+        this.formulaParser = formulaParser;
     }
 
     public void run() {
-        String[] values = inputView.formulaInput();
+        String formula = inputView.formulaInput();
+        String[] values = formulaParser.parse(formula);
         int result = operator.calculate(values);
         outputView.resultOutput(result);
     }
