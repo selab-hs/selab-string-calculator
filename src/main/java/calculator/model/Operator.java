684c9b9a2f4bd025de23130ce89cb8a1bb55251e
kangs <sangmoonkang@naver.com>
2024-08-30 22:48:13 +0900

    refactor(Calculator) : OperatorType Enum 추가 및 연산 처리 수정

--- src/main/java/calculator/model/Operator.java
+++ src/main/java/calculator/model/Operator.java
@@ -1,28 +1,32 @@
+package calculator.model;
+
 public class Operator {
-    public static int calculate(String[] values) {
-        Operations operations = new Operations();
+    private final Operations operations;
+
+    public Operator() {
+        this.operations = new Operations();
+    }
+
+    public int calculate(String[] values) {
         int result = Integer.parseInt(values[0]);
 
         for (int i = 1; i < values.length; i += 2) {
-            String operator = values[i];
+            OperatorType operatorType = OperatorType.fromString(values[i]);
             int nextNumber = Integer.parseInt(values[i + 1]);
 
-            switch (operator) {
-                case "+":
-                    result = operations.add(result, nextNumber);
+            switch (operatorType) {
+                case ADDITION:
+                    result = operations.addition(result, nextNumber);
                     break;
-                case "-":
+                case SUBTRACT:
                     result = operations.subtract(result, nextNumber);
                     break;
-                case "*":
+                case MULTIPLY:
                     result = operations.multiply(result, nextNumber);
                     break;
-                case "/":
+                case DIVIDE:
                     result = operations.divide(result, nextNumber);
                     break;
-                default:
-                    System.out.println("잘못된 연산자입니다.");
-                    return 0;
             }
         }
         return result;
