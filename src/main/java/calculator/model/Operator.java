7a44f528e03af6314a6c327cf1b7f4d1ad762336
kangs <sangmoonkang@naver.com>
2024-08-31 01:52:36 +0900

    refactor(Calculator) : Operations 의존성 제거

--- src/main/java/calculator/model/Operator.java
+++ src/main/java/calculator/model/Operator.java
@@ -1,34 +1,17 @@
 package calculator.model;
 
 public class Operator {
-    private final Operations operations;
-
-    public Operator() {
-        this.operations = new Operations();
-    }
 
     public int calculate(String[] values) {
         int result = Integer.parseInt(values[0]);
 
         for (int i = 1; i < values.length; i += 2) {
-            OperatorType operatorType = OperatorType.fromString(values[i]);
+            String symbol = values[i];
             int nextNumber = Integer.parseInt(values[i + 1]);
-
-            switch (operatorType) {
-                case ADDITION:
-                    result = operations.addition(result, nextNumber);
-                    break;
-                case SUBTRACT:
-                    result = operations.subtract(result, nextNumber);
-                    break;
-                case MULTIPLY:
-                    result = operations.multiply(result, nextNumber);
-                    break;
-                case DIVIDE:
-                    result = operations.divide(result, nextNumber);
-                    break;
-            }
+            OperatorType operatorType = OperatorType.fromString(symbol);
+            result = operatorType.apply(result, nextNumber);
         }
+
         return result;
     }
 }
